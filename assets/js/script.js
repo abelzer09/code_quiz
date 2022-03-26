8103775790d7ded9bb8a285e20949d35a664b63f
abelzer09 <aaronrbelzer@gmail.com>
2022-03-26 17:43:39 -0500

    added function to remove rules and start button once game is started

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,35 @@ var answersEl = document.getElementById("answers");
 var startBtn = document.getElementById("start");
 var questionsEl = document.getElementById("questions");
 var counter = 0;
-var score = 0;
+var scoreCounter = 0;
 var time = 5;
 var head = document.getElementById("header");
+var base = false;
+
+function gameStart() {
+    if (rulesEl.style.display === "none") {
+        rulesEl.style.display = "block";
+    } else {
+        rulesEl.style.display = "none";
+    }
+    if (startBtn.style.display === "none") {
+        startBtn.style.display = "block";
+    } else {
+        startBtn.style.display = "none";
+    }
+}
 
 
-startBtn.addEventListener("click",function(){
-    var gameTimer = setInterval(function(){
+startBtn.addEventListener("click", function () {
+    gameStart()
+    var gameTimer = setInterval(function () {
         time--;
         if (time >= 0) {
             var ptag = document.createElement("p");
             ptag.textContent = "Time: " + time;
             timerEl.innerHTML = "";
             timerEl.appendChild(ptag);
-        }else{
+        } else {
             clearInterval(gameTimer);
             alert("time up");
         }
