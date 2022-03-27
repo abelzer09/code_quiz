ec74bef2e1bdbf6ea00ddc3c58afa5b0065ad0e7
abelzer09 <aaronrbelzer@gmail.com>
2022-03-27 10:47:29 -0500

    added quiz question var, and functions for game questions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,53 @@ var scoreCounter = 0;
 var time = 5;
 var head = document.getElementById("header");
 var base = false;
+var points = 15;
+var quizQuestion = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices:["javescript", "script", "scripting", "js"], 
+        answer: "script"
+    },{
+        
+            question: "question2",
+            choices:["a", "b", "c", "d"], 
+            answer: "d"
+        
+    },{
+        
+        question: "question3",
+        choices:["a", "b", "c", "d"], 
+        answer: "c"
+    
+}];
+
+function startQuiz() {
+    var gameTimer = setInterval(function() {
+        time--;
+        if (time >= 0) {
+            var ptag = document.createElement("p");
+            ptag.textContent = "Time: " + time;
+            timerEl.innerHTML = "";
+            timerEl.appendChild(ptag);
+        } else {
+            clearInterval(gameTimer);
+            alert("time up");
+        }
+    }, 1000);
+    gameStart();
+    questionsEl.innerHTML = `
+    <h3>${quizQuestion[counter].question}</h3>
+    <button class="choices" value="${quizQuestion[counter].choices[0]}">${quizQuestion[counter].choices[0]}</button>
+    <button class="choices" value="${quizQuestion[counter].choices[1]}">${quizQuestion[counter].choices[1]}</button>
+    <button class="choices" value="${quizQuestion[counter].choices[2]}">${quizQuestion[counter].choices[2]}</button>
+    <button class="choices" value="${quizQuestion[counter].choices[3]}">${quizQuestion[counter].choices[3]}</button>
+    `;
+    var choices =  document.querySelectorAll(".choices")
+    for (var i=0; i < choices.length; i++){
+        choices[i].addEventListener("click",nextQuestion)
+    }
+}
+
 
 function gameStart() {
     if (rulesEl.style.display === "none") {
@@ -24,18 +71,4 @@ function gameStart() {
 }
 
 
-startBtn.addEventListener("click", function () {
-    gameStart()
-    var gameTimer = setInterval(function () {
-        time--;
-        if (time >= 0) {
-            var ptag = document.createElement("p");
-            ptag.textContent = "Time: " + time;
-            timerEl.innerHTML = "";
-            timerEl.appendChild(ptag);
-        } else {
-            clearInterval(gameTimer);
-            alert("time up");
-        }
-    }, 1000);
-})
+startBtn.addEventListener("click", startQuiz)
