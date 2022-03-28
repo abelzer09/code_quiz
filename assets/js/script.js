057ab685bac564306a212fb60d0244982a4cc2fa
abelzer09 <aaronrbelzer@gmail.com>
2022-03-28 17:33:59 -0500

    created a function for intial page set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,67 +14,59 @@ var base = false;
 var points = 5;
 var leader = document.getElementById("leaderBoard");
 
-questionsEl.setAttribute("class","hide")
-rulesEl.setAttribute("class", "show")
-highScorePage.setAttribute("class","hide")
-leader.setAttribute("class","hide")
-head.setAttribute("class","show")
+inti();
 
 var quizQuestion = [
     {
         question: "Inside which HTML element do we put the JavaScript?",
-        choices:["javescript", "script", "scripting", "js"], 
+        choices: ["javescript", "script", "scripting", "js"],
         answer: "script"
-    },{
-        
-            question: "question2",
-            choices:["a", "b", "c", "d"], 
-            answer: "d"
-        
-    },{
-        
-        question: "question3",
-        choices:["a", "b", "c", "d"], 
-        answer: "c"
-    
-}];
+    }, {
 
-function resetGame (){
-    questionsEl.setAttribute("class","hide")
-    rulesEl.setAttribute("class", "show")
-    highScorePage.setAttribute("class","hide")
-    leader.setAttribute("class","hide")
-    head.setAttribute("class","show")
+        question: "question2",
+        choices: ["a", "b", "c", "d"],
+        answer: "d"
+
+    }, {
+
+        question: "question3",
+        choices: ["a", "b", "c", "d"],
+        answer: "c"
+
+    }];
+
+function resetGame() {
+    inti();
 }
 
-function scoreBoard (){
+function scoreBoard() {
     scoreEl.textContent = "Score: " + scoreCounter;
 }
 
 document.getElementById("backBtn").addEventListener("click", resetGame);
 
-document.getElementById("submit").addEventListener("click", function(e){
+document.getElementById("submit").addEventListener("click", function (e) {
     // alert("working")
     var userInitials = document.getElementById("initials").value;
-    leader.setAttribute("class","show")
-    highScorePage.setAttribute("class","hide")
+    leader.setAttribute("class", "show")
+    highScorePage.setAttribute("class", "hide")
 
     console.log(userInitials);
 })
 
-function nextQuestion(event){
-    if (quizQuestion[counter].answer == event.target.value){
+function nextQuestion(event) {
+    if (quizQuestion[counter].answer == event.target.value) {
         scoreCounter += 15
         console.log(scoreCounter)
     }
-    else{
+    else {
         time -= 15
     }
     counter++;
-    if (counter == quizQuestion.length){
+    if (counter == quizQuestion.length) {
         highScore();
     }
-    
+
     scoreBoard();
     questionsEl.innerHTML = `
     <h3>${quizQuestion[counter].question}</h3>
@@ -83,23 +75,23 @@ function nextQuestion(event){
     <button class="choices" value="${quizQuestion[counter].choices[2]}">${quizQuestion[counter].choices[2]}</button>
     <button class="choices" value="${quizQuestion[counter].choices[3]}">${quizQuestion[counter].choices[3]}</button>
     `;
-    var choices =  document.querySelectorAll(".choices")
-    for (var i=0; i < choices.length; i++){
-        choices[i].addEventListener("click",nextQuestion)
+    var choices = document.querySelectorAll(".choices")
+    for (var i = 0; i < choices.length; i++) {
+        choices[i].addEventListener("click", nextQuestion)
     }
 }
 
 
 
-function highScore(){
-    highScorePage.setAttribute("class","show")
-    questionsEl.setAttribute("class","hide")
-    head.setAttribute("class","hide")
+function highScore() {
+    highScorePage.setAttribute("class", "show")
+    questionsEl.setAttribute("class", "hide")
+    head.setAttribute("class", "hide")
 
 }
 
 function startQuiz() {
-    var gameTimer = setInterval(function() {
+    var gameTimer = setInterval(function () {
         time--;
         if (time >= 0) {
             var ptag = document.createElement("p");
@@ -120,17 +112,24 @@ function startQuiz() {
     <button class="choices" value="${quizQuestion[counter].choices[2]}">${quizQuestion[counter].choices[2]}</button>
     <button class="choices" value="${quizQuestion[counter].choices[3]}">${quizQuestion[counter].choices[3]}</button>
     `;
-    var choices =  document.querySelectorAll(".choices")
-    for (var i=0; i < choices.length; i++){
-        choices[i].addEventListener("click",nextQuestion)
+    var choices = document.querySelectorAll(".choices")
+    for (var i = 0; i < choices.length; i++) {
+        choices[i].addEventListener("click", nextQuestion)
     }
 }
 
 
 function gameStart() {
-   rulesEl.setAttribute("class", "hide")
-   questionsEl.setAttribute("class","show")
+    rulesEl.setAttribute("class", "hide")
+    questionsEl.setAttribute("class", "show")
 }
 
+function inti(){
+    questionsEl.setAttribute("class", "hide")
+    rulesEl.setAttribute("class", "show")
+    highScorePage.setAttribute("class", "hide")
+    leader.setAttribute("class", "hide")
+    head.setAttribute("class", "show")
+}
 
 startBtn.addEventListener("click", startQuiz)
